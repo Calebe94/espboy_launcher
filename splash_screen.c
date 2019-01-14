ea28ce2965752315923c8e66ebfe49cb5a97b394
Calebe94 <calebe.sep@hotmail.com>
2019-01-14 15:26:17 -0200

    feat: splash screen done, should merge to master

--- splash_screen.c
+++ splash_screen.c
@@ -15,25 +15,33 @@
 #include "splash_screen.h"
 
 #include "minos_logo.h"
+
 /**********************
  *   MACROS
  **********************/
-#define USE_ANIMATED_ARC    0
 #define USE_BACKGROUND      1
-/**********************
- *   GLOBAL VARIABLES
- **********************/
-lv_obj_t * arc;
 
 /**********************
- *   STATIC FUNCTIONS
+ *   STATIC VARIABLES
  **********************/
-// void lv_obj_set_height(lv_obj_t * obj, lv_coord_t h);
+/* OBJECTS */
+static lv_obj_t * preload;
+static lv_obj_t * background;
 
-static void lv_obj_set_angle(lv_obj_t * obj, int value)
-{
-    lv_arc_set_angles(obj, 90, value);
-}
+/* STYLES */
+static lv_style_t style_background;
+static lv_style_t style_preload;
+
+/********************************
+ *   STATIC FUNCTIONS PROTOTYPES
+ *******************************/
+/* STYLES */
+static void styles_init(void);
+
+static void set_styles(void);
+
+/* OBJECTS */
+static void create_splashscreen(void);
 
 /**********************
  *   GLOBAL FUNCTIONS
@@ -44,76 +52,83 @@ static void lv_obj_set_angle(lv_obj_t * obj, int value)
  */
 void splash_screen_init()
 {
-    
-    #if USE_BACKGROUND
-        lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
-        lv_img_set_src(img1, &minos_logo);
-        lv_obj_set_width(img1, minos_logo.header.w);
-        lv_obj_set_height(img1, minos_logo.header.h);
-        lv_obj_align(img1, NULL, LV_ALIGN_CENTER, 0, 0);
+    styles_init();
 
-        static lv_style_t style_img3;
-        lv_style_copy(&style_img3, &lv_style_plain);
-        // style_img3.image.color = LV_COLOR_HEX(0x003b75);
-        style_img3.body.opa = LV_OPA_10;
+    create_splashscreen();
 
-        lv_obj_set_style(img1, &style_img3);
-    #endif
-    lv_obj_t * splash_screen_mbox = lv_mbox_create(img1, NULL);
-    lv_obj_align(splash_screen_mbox, NULL, LV_ALIGN_CENTER, 0, -40);
-    lv_obj_set_style(splash_screen_mbox, &style_img3);
-    lv_mbox_set_text(splash_screen_mbox, "Loading...");
-
-    #if USE_ANIMATED_ARC
-        static lv_style_t style;
-        lv_style_copy(&style, &lv_style_plain);
-        style.line.color = LV_COLOR_BLUE;           /*Arc color*/
-        style.line.width = 8;                       /*Arc width*/
-
-
-        /*Copy the previous Arc and set different angles and size*/
-        arc = lv_arc_create(splash_screen_mbox, NULL);
-        lv_arc_set_style(arc, LV_ARC_STYLE_MAIN, &style);          /*Use the new style*/
-        lv_arc_set_angles(arc, 90, 0);
-        lv_obj_set_size(arc, 100, 100);
-        lv_obj_align(arc, splash_screen_mbox, LV_ALIGN_CENTER, 0, 0);
-
-        lv_anim_t a;                                
-        a.var = arc;                             /*Variable to animate*/                        
-        a.start = 0;                                 /*Start value*/
-        a.end = 360;                                 /*End value*/
-        a.fp = (lv_anim_fp_t)lv_obj_set_angle;                                 /*Function to be used to animate*/
-        a.path = lv_anim_path_linear;                                            /*Path of animation*/
-        a.end_cb = NULL;                               /*Callback when the animation is ready*/
-        a.act_time = 0;                                 /*Set < 0 to make a delay [ms]*/
-        a.time = 2000;                                 /*Animation length [ms]*/
-        a.playback = 0;                                 /*1: animate in reverse direction too when the normal is ready*/
-        a.playback_pause = 0;                                 /*Wait before playback [ms]*/
-        a.repeat = 1;                                 /*1: Repeat the animation (with or without playback)*/
-        a.repeat_pause = 0;                             /*Wait before repeat [ms]*/
-
-        lv_anim_create(&a);                          /*Start the animation*/
-    #else
-        static lv_style_t style;
-        lv_style_copy(&style, &lv_style_plain);
-        style.line.width = 10;                         /*10 px thick arc*/
-        style.line.color = LV_COLOR_HEX3(0x258);       /*Blueish arc color*/
-
-        style.body.border.color = LV_COLOR_HEX3(0xBBB); /*Gray background color*/
-        style.body.border.width = 10;
-        style.body.padding.hor = 0;
-
-        /*Create a Preloader object*/
-        lv_obj_t * preload = lv_preload_create(splash_screen_mbox, NULL);
-        lv_obj_set_size(preload, 100, 100);
-        lv_obj_align(preload, NULL, LV_ALIGN_CENTER, 0, 0);
-        lv_preload_set_style(preload, LV_PRELOAD_STYLE_MAIN, &style);
-    #endif
+    set_styles();
 } 
 
 /*!
  * @brief Creates and deinitialize the ESPBoy Splash Screen
  */
-void splash_screen_deinit();
+void splash_screen_deinit()
+{
+    lv_obj_clean(preload);
+    lv_obj_clean(background);
+    lv_obj_del(preload);
+    lv_obj_del(background);
+}
+
+
+/*********************
+ *   STATIC FUNCTIONS 
+ *********************/
+/* STYLES */
+static void styles_init(void)
+{
+    /* PRELOAD STYLE */
+    lv_style_copy(&style_preload, &lv_style_plain);
+    style_preload.body.padding.hor = 0;
+    style_preload.line.color = LV_COLOR_BLACK;
+    style_preload.body.border.color = LV_COLOR_GRAY; 
+
+    #if USE_BACKGROUND
+        /* BACKGROUND STYLE */
+        lv_style_copy(&style_background, &lv_style_plain);
+        style_background.body.opa = LV_OPA_10;
+        /* PRELOAD STYLE */
+        style_preload.line.width = 14;
+        style_preload.body.border.width = 14;
+    #else
+        style_preload.line.width = 15;
+        style_preload.body.border.width = 15;
+    #endif
+
+}
+
+static void set_styles(void)
+{
+    #if  USE_BACKGROUND
+        lv_obj_set_style(background, &style_background);
+    #endif
+
+    lv_preload_set_style(preload, LV_PRELOAD_STYLE_MAIN, &style_preload);
+}
+
+/* OBJECTS */
+static void create_splashscreen(void)
+{
+    #if USE_BACKGROUND
+        /* CREATE BACKGROUND IMAGE */
+        background = lv_img_create(lv_scr_act(), NULL);
+        preload = lv_preload_create(lv_scr_act(), NULL);
+        lv_img_set_src(background, &minos_logo);
+        lv_obj_set_width(background, minos_logo.header.w);
+        lv_obj_set_height(background, minos_logo.header.h);
+        lv_obj_align(background, NULL, LV_ALIGN_CENTER, 0, 0);
+        /*Create a Preloader object*/
+        lv_obj_set_size(preload, 50, 50);
+        lv_obj_align(preload, NULL, LV_ALIGN_IN_TOP_RIGHT, -104, 24);
+    #else
+        preload = lv_preload_create(lv_scr_act(), NULL);
+        lv_obj_set_size(preload, 115, 115);
+        lv_obj_align(preload, NULL, LV_ALIGN_CENTER, 0, 0);
+        lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+        lv_label_set_text(label, "Loading...");
+        lv_obj_align(label, preload, LV_ALIGN_CENTER, 0,0);
+    #endif
+}
+
 
 /*** end of file ***/
